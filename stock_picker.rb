28c95e7a4d7b0180d816f24c7a5dbd7e17550c52
Amanda <125668669+amcconnell1316@users.noreply.github.com>
2023-09-24 17:50:46 -0500

    Stock Picker Solution

---
+++ stock_picker.rb
@@ -0,0 +1,22 @@
+def stock_picker(prices)
+    low_index=0
+    high_index=0
+    prices.each_with_index do |price_low, i_low_index|
+        prices.each_with_index do |price_high, i_high_index|
+            if i_high_index > i_low_index 
+                if price_high - price_low > prices[high_index] - prices[low_index]
+                    low_index = i_low_index
+                    high_index = i_high_index
+                end
+            end
+        end
+    end
+    return [low_index, high_index]
+end
+
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([1,3,6,9,15,8,6,1,17])
+p stock_picker([17,3,6,9,2,18,6,10,1])
+p stock_picker([1,13,6,9,15,8,6,1,12])
+p stock_picker([1,17])
