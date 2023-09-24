ed449e2ad8ed042c300d876a02688990c2860e4c
Amanda <125668669+amcconnell1316@users.noreply.github.com>
2023-09-23 20:21:03 -0500

    Solution

---
+++ caesar_cipher.rb
@@ -0,0 +1,18 @@
+def encode(string, numShift)
+    chars = string.bytes()
+    newChars = chars.map do |char|
+        newChar = char
+        if char >= 65 && char <=90 
+            newChar = char + numShift
+            newChar -= 26 if newChar > 90
+        elsif char >= 97 && char <= 122
+            newChar = char + numShift
+            newChar -= 26 if newChar > 122    
+        end
+        newChar
+    end
+    newChars.pack('c*')
+end
+
+puts encode("ABCabcXYZxyz",1)
+puts encode("Hello World!",10)
